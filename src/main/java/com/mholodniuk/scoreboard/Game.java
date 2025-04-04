fc0c82b3c8cef7768196ad655462fcd2ff5a68e5
maciejholodniuk <maciekholo@gmail.com>
2025-04-05 00:01:37 +0200

    add Game sorting tests and implementation

--- src/main/java/com/mholodniuk/scoreboard/Game.java
+++ src/main/java/com/mholodniuk/scoreboard/Game.java
@@ -2,7 +2,10 @@ package com.mholodniuk.scoreboard;
 
 import com.mholodniuk.scoreboard.vo.Score;
 import com.mholodniuk.scoreboard.vo.Team;
+import com.mholodniuk.scoreboard.vo.util.Pair;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
 
 public final class Game {
     private final Team homeTeam;
@@ -33,5 +36,23 @@ public final class Game {
 
     public Team awayTeam() {
         return awayTeam;
+    }
+
+    @Override
+    public String toString() {
+        return homeTeam.name() + " " + score.homeScore() + " - " + awayTeam.name() + " " + score.homeScore();
+    }
+}
+
+class GameAndInsertionTimeComparator implements Comparator<Pair<LocalDateTime, Game>> {
+    @Override
+    public int compare(Pair<LocalDateTime, Game> game1, Pair<LocalDateTime, Game> game2) {
+        int scoreComparison = Integer.compare(game2.second().score().total(), game1.second().score().total());
+
+        if (scoreComparison != 0) {
+            return scoreComparison;
+        }
+
+        return game2.first().compareTo(game1.first());
     }
 }
--- src/test/java/com/mholodniuk/scoreboard/GameTest.java
+++ src/test/java/com/mholodniuk/scoreboard/GameTest.java
@@ -2,7 +2,11 @@ package com.mholodniuk.scoreboard;
 
 import com.mholodniuk.scoreboard.vo.Score;
 import com.mholodniuk.scoreboard.vo.Team;
+import com.mholodniuk.scoreboard.vo.util.Pair;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,5 +31,45 @@ class GameTest {
         game.updateScore(newScore);
 
         assertEquals(newScore, game.score());
+    }
+
+    @Test
+    void test_GamesAreSortableByTotalScore() {
+        var game1 = createSimpleGame("Team 1", "Team 2", 2, 1);
+        var game2 = createSimpleGame("Team 3", "Team 4", 0, 0);
+        var insertionTime = LocalDateTime.now();
+
+        var games = Arrays.asList(Pair.of(insertionTime, game1), Pair.of(insertionTime, game2));
+
+        games.sort(new GameAndInsertionTimeComparator());
+
+        var sortedGames = games.stream().map(Pair::second).toList();
+
+        assertEquals(game1, sortedGames.get(0));
+        assertEquals(game2, sortedGames.get(1));
+    }
+
+    @Test
+    void test_GamesAreSortableByInsertionTimeIfTotalScoresAreEqual() {
+        var game1 = createSimpleGame("Team 1", "Team 2", 5, 5);
+        var game2 = createSimpleGame("Team 3", "Team 4", 10, 0);
+        var now = LocalDateTime.now();
+        var game1InsertionTime = now.plusSeconds(10);
+        var game2InsertionTime = now.plusSeconds(1);
+
+        var games = Arrays.asList(Pair.of(game1InsertionTime, game1), Pair.of(game2InsertionTime, game2));
+
+        games.sort(new GameAndInsertionTimeComparator());
+
+        var sortedGames = games.stream().map(Pair::second).toList();
+
+        assertEquals(game2, sortedGames.get(1));
+        assertEquals(game1, sortedGames.get(0));
+    }
+
+    private Game createSimpleGame(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        var game = new Game(new Team(homeTeam), new Team(awayTeam));
+        game.updateScore(new Score(homeScore, awayScore));
+        return game;
     }
 }
