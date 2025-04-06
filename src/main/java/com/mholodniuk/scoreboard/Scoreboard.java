4dd2252b7955ad6f51c9d1748b87771980d60d38
maciejholodniuk <maciekholo@gmail.com>
2025-04-06 19:53:56 +0200

    Scoreboard implementation #3: move comparator to Scoreboard, improve exceptions and naming

--- src/main/java/com/mholodniuk/scoreboard/Scoreboard.java
+++ src/main/java/com/mholodniuk/scoreboard/Scoreboard.java
@@ -3,7 +3,7 @@ package com.mholodniuk.scoreboard;
 import com.mholodniuk.scoreboard.vo.Score;
 import com.mholodniuk.scoreboard.vo.ScoreboardSummary;
 import com.mholodniuk.scoreboard.vo.Team;
-import com.mholodniuk.scoreboard.vo.util.Pair;
+import com.mholodniuk.scoreboard.util.Pair;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -12,46 +12,64 @@ import static com.mholodniuk.scoreboard.util.NullSafetyUtils.requireNonNull;
 import static com.mholodniuk.scoreboard.util.StreamUtils.findFirstMatchingOrNull;
 
 public class Scoreboard {
-    private final List<Pair<LocalDateTime, Game>> activeGames = new ArrayList<>();
-    private final Set<Team> activeTeams = new HashSet<>();
+    private final List<Pair<Game, LocalDateTime /* Insertion time */>> trackedGames = new ArrayList<>();
+    private final Set<Team> playingTeams = new HashSet<>();
 
     public Game startGame(Team homeTeam, Team awayTeam) {
-        requireNonNull(homeTeam, awayTeam);
+        requireNonNull("Teams must not be null", homeTeam, awayTeam);
         if (Objects.equals(homeTeam, awayTeam)) {
             throw new IllegalArgumentException("You cannot start a game with the same team");
         }
-        if (activeTeams.contains(homeTeam) || activeTeams.contains(awayTeam)) {
-            throw new IllegalStateException("One or both teams are already in an active game");
+        if (playingTeams.contains(homeTeam) || playingTeams.contains(awayTeam)) {
+            throw new IllegalStateException("One or both teams are already playing");
         }
         Game game = new Game(homeTeam, awayTeam);
-        activeGames.add(Pair.of(LocalDateTime.now(), game));
-        activeTeams.addAll(List.of(homeTeam, awayTeam));
+        trackedGames.add(Pair.of(game, LocalDateTime.now()));
+        playingTeams.addAll(List.of(homeTeam, awayTeam));
         return game;
     }
 
     public void updateGameScore(Game game, Score score) {
-        requireNonNull(game, score);
+        requireNonNull("Game must not be null", game);
+        requireNonNull("Score must not be null", score);
         for (var activeGame : activeGames()) {
             if (Objects.equals(game, activeGame)) {
                 game.updateScore(score);
+                return;
             }
         }
     }
 
     public void finishGame(Game game) {
-        requireNonNull(game);
-        var gameToFinish = findFirstMatchingOrNull(activeGames, (insertionTimeAndGamePair) -> Objects.equals(game, insertionTimeAndGamePair.second()));
-        if (activeGames.remove(gameToFinish)) {
-            game.teams().forEach(activeTeams::remove);
+        requireNonNull("Game must not be null", game);
+        var gameToFinish = findFirstMatchingOrNull(trackedGames, trackedGame -> Objects.equals(game, trackedGame.first()));
+        if (trackedGames.remove(gameToFinish)) {
+            game.teams().forEach(playingTeams::remove);
         }
     }
 
     public ScoreboardSummary collectSummary() {
-        var orderedGames = activeGames.stream().sorted(new GameAndInsertionTimeComparator()).map(Pair::second).toList();
+        var orderedGames = trackedGames.stream()
+                .sorted(new GameAndInsertionTimeComparator()).map(Pair::first)
+                .toList();
+
         return ScoreboardSummary.of(orderedGames);
     }
 
     private List<Game> activeGames() {
-        return activeGames.stream().map(Pair::second).toList();
+        return trackedGames.stream().map(Pair::first).toList();
+    }
+
+    private static class GameAndInsertionTimeComparator implements Comparator<Pair<Game, LocalDateTime>> {
+        @Override
+        public int compare(Pair<Game, LocalDateTime> game1, Pair<Game, LocalDateTime>game2) {
+            int scoreComparison = Integer.compare(game2.first().score().total(), game1.first().score().total());
+
+            if (scoreComparison != 0) {
+                return scoreComparison;
+            }
+
+            return game2.second().compareTo(game1.second());
+        }
     }
 }
--- src/test/java/com/mholodniuk/scoreboard/GameTest.java
+++ src/test/java/com/mholodniuk/scoreboard/GameTest.java
@@ -2,13 +2,9 @@ package com.mholodniuk.scoreboard;
 
 import com.mholodniuk.scoreboard.vo.Score;
 import com.mholodniuk.scoreboard.vo.Team;
-import com.mholodniuk.scoreboard.vo.util.Pair;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameTest {
 
@@ -18,7 +14,7 @@ class GameTest {
         var team2 = new Team("Team 2");
         var game = new Game(team1, team2);
 
-        assertEquals(Score.initialize(), game.score());
+        assertEquals(new Score(0, 0), game.score());
     }
 
     @Test
@@ -31,45 +27,5 @@ class GameTest {
         game.updateScore(newScore);
 
         assertEquals(newScore, game.score());
-    }
-
-    @Test
-    void test_GamesAreSortableByTotalScore() {
-        var game1 = createSimpleGame("Team 1", "Team 2", 2, 1);
-        var game2 = createSimpleGame("Team 3", "Team 4", 0, 0);
-        var insertionTime = LocalDateTime.now();
-
-        var games = Arrays.asList(Pair.of(insertionTime, game1), Pair.of(insertionTime, game2));
-
-        games.sort(new GameAndInsertionTimeComparator());
-
-        var sortedGames = games.stream().map(Pair::second).toList();
-
-        assertEquals(game1, sortedGames.get(0));
-        assertEquals(game2, sortedGames.get(1));
-    }
-
-    @Test
-    void test_GamesAreSortableByInsertionTimeIfTotalScoresAreEqual() {
-        var game1 = createSimpleGame("Team 1", "Team 2", 5, 5);
-        var game2 = createSimpleGame("Team 3", "Team 4", 10, 0);
-        var now = LocalDateTime.now();
-        var game1InsertionTime = now.plusSeconds(10);
-        var game2InsertionTime = now.plusSeconds(1);
-
-        var games = Arrays.asList(Pair.of(game1InsertionTime, game1), Pair.of(game2InsertionTime, game2));
-
-        games.sort(new GameAndInsertionTimeComparator());
-
-        var sortedGames = games.stream().map(Pair::second).toList();
-
-        assertEquals(game2, sortedGames.get(1));
-        assertEquals(game1, sortedGames.get(0));
-    }
-
-    private Game createSimpleGame(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-        var game = new Game(new Team(homeTeam), new Team(awayTeam));
-        game.updateScore(new Score(homeScore, awayScore));
-        return game;
     }
 }
