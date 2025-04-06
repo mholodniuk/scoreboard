fb63702c92a65b10bf082b5323644580711d0420
maciejholodniuk <maciekholo@gmail.com>
2025-04-06 12:52:44 +0200

    create test implementation for Scoreboard

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Assumptions
+
+## Design
 - game's state (other than score) does not matter
 - game's score values won't be insanely large
+- game's score can be manipulated in every way (goals can be withdrawn or added two or more at a time)
 - games are unique in one scoreboard (e.g. there cannot be two games for any team happening at once)
-- 
+- there can be multiple scoreboards associated with multiple games, so:
+  - only games that are within the scope of a given scoreboard can be affected by the scoreboard operations (e.g. scoreboard 1 cannot update games coming from scoreboard 2)
+  - games that are untracked by given scoreboard are ignored
+
+## Technical
+- Game can be only modified and initialized using Scoreboard
+- Games are identified by their memory address (no ID is used)
--- src/test/java/com/mholodniuk/scoreboard/ScoreboardTest.java
+++ src/test/java/com/mholodniuk/scoreboard/ScoreboardTest.java
@@ -1,7 +1,11 @@
 package com.mholodniuk.scoreboard;
 
+import com.mholodniuk.scoreboard.vo.Score;
+import com.mholodniuk.scoreboard.vo.Team;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreboardTest {
 
@@ -9,56 +13,236 @@ class ScoreboardTest {
     class CreateGameTest {
         @Test
         void test_StartGameShouldAddNewValidGame() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var scoreboard = new Scoreboard();
 
+            scoreboard.startGame(team1, team2);
+
+            var scoreboardSummary = scoreboard.collectSummary();
+            assertEquals(1, scoreboardSummary.games().size());
+
+            var game = scoreboardSummary.games().getFirst();
+            assertEquals(team1, game.homeTeam());
+            assertEquals(team2, game.awayTeam());
+            assertEquals(new Score(0, 0), game.score());
         }
 
         @Test
-        void test_StartGameShouldNotAllowDuplicateGames() {
+        void test_StartGameShouldNotAllowDuplicateTeamGames() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var team3 = new Team("Team 3");
+            var scoreboard = new Scoreboard();
 
+            scoreboard.startGame(team1, team2);
+
+            assertThrows(IllegalStateException.class, () -> scoreboard.startGame(team1, team3));
         }
 
         @Test
-        void test_StartGameShouldThrowWhenTeamsAreNull() {
+        void test_StartGameShouldThrowWhenEitherTeamIsNullOrEmpty() {
+            var team = new Team("Non-null Team");
+            var scoreboard = new Scoreboard();
 
+            assertThrows(IllegalStateException.class, () -> scoreboard.startGame(null, null));
+            assertThrows(IllegalStateException.class, () -> scoreboard.startGame(team, null));
+            assertThrows(IllegalStateException.class, () -> scoreboard.startGame(null, team));
         }
 
         @Test
         void test_StartGameShouldNotAllowSameTeamForHomeAndAway() {
+            var team = new Team("Team");
+            var scoreboard = new Scoreboard();
 
+            assertThrows(IllegalArgumentException.class, () -> scoreboard.startGame(team, team));
         }
     }
 
     @Nested
     class UpdateGameTest {
+        @Test
         void test_UpdateGameScoreShouldUpdateSuccessfullyForTrackedGame() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var scoreboard = new Scoreboard();
+            var startedGame = scoreboard.startGame(team1, team2);
 
+            scoreboard.updateGameScore(startedGame, new Score(1, 0));
+
+            var scoreboardSummary = scoreboard.collectSummary();
+            assertEquals(1, scoreboardSummary.games().size());
+
+            var game = scoreboardSummary.games().getFirst();
+            assertEquals(new Score(1, 0), game.score());
         }
 
+        @Test
+        void test_UpdateGameScoreShouldUpdateAllowMultipleUpdatesForTrackedGame() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var scoreboard = new Scoreboard();
+            var startedGame = scoreboard.startGame(team1, team2);
+
+            scoreboard.updateGameScore(startedGame, new Score(1, 0));
+            scoreboard.updateGameScore(startedGame, new Score(1, 1));
+            scoreboard.updateGameScore(startedGame, new Score(1, 2));
+
+            var scoreboardSummary = scoreboard.collectSummary();
+            assertEquals(1, scoreboardSummary.games().size());
+
+            var game = scoreboardSummary.games().getFirst();
+            assertEquals(new Score(1, 2), game.score());
+        }
+
+        @Test
         void test_UpdateGameScoreShouldFailForUntrackedGame() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var scoreboard = new Scoreboard();
+            var untrackedGame = new Game(team1, team2); // untracked game might come from different scoreboard
 
+            scoreboard.updateGameScore(untrackedGame, new Score(10, 0));
+
+            var scoreboardSummary = scoreboard.collectSummary();
+            assertEquals(0, scoreboardSummary.games().size());
         }
 
-        void test_UpdateGameScoreShouldRejectNegativeValues() {
-
+        @Test
+        void test_UpdateGameScoreShouldFailForNullGame() {
+            assertThrows(IllegalArgumentException.class, () -> new Scoreboard().updateGameScore(null, new Score(10, 0)));
         }
 
-        void test_UpdateGameScoreShouldAllowSameScoreUpdate() {
+        @Test
+        void test_UpdateGameScoreShouldFailForNullScore() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var scoreboard = new Scoreboard();
+            var startedGame = scoreboard.startGame(team1, team2);
 
+            assertThrows(IllegalArgumentException.class, () -> scoreboard.updateGameScore(startedGame, null));
         }
     }
 
     @Nested
     class FinishGameTest {
+        @Test
         void test_FinishGameShouldRemoveTrackedGame() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var scoreboard = new Scoreboard();
+            var startedGame = scoreboard.startGame(team1, team2);
 
+            var scoreboardSummaryBeforeFinishing = scoreboard.collectSummary();
+            assertEquals(1, scoreboardSummaryBeforeFinishing.games().size());
+
+            scoreboard.finishGame(startedGame);
+
+            var scoreboardSummary = scoreboard.collectSummary();
+            assertEquals(0, scoreboardSummary.games().size());
         }
 
+        @Test
         void test_FinishGameShouldIgnoreUntrackedGame() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var team3 = new Team("Team 3");
+            var team4 = new Team("Team 4");
+            var scoreboard = new Scoreboard();
+            scoreboard.startGame(team1, team2);
+            var untrackedGame = new Game(team3, team4); // untracked game might come from different scoreboard
 
+            var scoreboardSummaryBeforeFinishing = scoreboard.collectSummary();
+            assertEquals(1, scoreboardSummaryBeforeFinishing.games().size());
+
+            scoreboard.finishGame(untrackedGame);
+
+            var scoreboardSummary = scoreboard.collectSummary();
+            assertEquals(1, scoreboardSummary.games().size());
         }
 
+        @Test
         void test_FinishGameShouldBeIdempotent() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var scoreboard = new Scoreboard();
+            var startedGame = scoreboard.startGame(team1, team2);
 
+            var scoreboardSummaryBeforeFinishing = scoreboard.collectSummary();
+            assertEquals(1, scoreboardSummaryBeforeFinishing.games().size());
+
+            scoreboard.finishGame(startedGame);
+            scoreboard.finishGame(startedGame);
+            scoreboard.finishGame(startedGame);
+
+            var scoreboardSummary = scoreboard.collectSummary();
+            assertEquals(0, scoreboardSummary.games().size());
+        }
+
+        @Test
+        void test_FinishGameShouldNotAllowNullGame() {
+            assertThrows(IllegalArgumentException.class, () -> new Scoreboard().finishGame(null));
+        }
+    }
+
+    @Nested
+    class GameSummaryTest {
+        @Test
+        void test_CollectSummaryShouldReturnEmptyWhenNoGames() {
+            var scoreboardSummary = new Scoreboard().collectSummary();
+            assertEquals(0, scoreboardSummary.games().size());
+        }
+
+        @Test
+        void test_CollectSummaryShouldOrderGamesByTotalScoreDescending() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var team3 = new Team("Team 3");
+            var team4 = new Team("Team 4");
+            var team5 = new Team("Team 5");
+            var team6 = new Team("Team 6");
+
+            var scoreboard = new Scoreboard();
+            var game1 = scoreboard.startGame(team1, team2);
+            var game2 = scoreboard.startGame(team3, team4);
+            var game3 = scoreboard.startGame(team5, team6);
+
+            scoreboard.updateGameScore(game1, new Score(1, 2));
+            scoreboard.updateGameScore(game2, new Score(2, 0));
+            scoreboard.updateGameScore(game3, new Score(3, 4));
+
+            var scoreboardSummary = scoreboard.collectSummary();
+
+            assertEquals(3, scoreboardSummary.games().size());
+            assertEquals(game3, scoreboardSummary.games().get(0));
+            assertEquals(game1, scoreboardSummary.games().get(1));
+            assertEquals(game2, scoreboardSummary.games().get(2));
+        }
+
+        @Test
+        void test_CollectSummaryShouldUseRecencyForGamesWithSameTotalScore() {
+            var team1 = new Team("Team 1");
+            var team2 = new Team("Team 2");
+            var team3 = new Team("Team 3");
+            var team4 = new Team("Team 4");
+            var team5 = new Team("Team 5");
+            var team6 = new Team("Team 6");
+
+            var scoreboard = new Scoreboard();
+            var game1 = scoreboard.startGame(team1, team2);
+            var game2 = scoreboard.startGame(team3, team4);
+            var game3 = scoreboard.startGame(team5, team6);
+
+            scoreboard.updateGameScore(game1, new Score(2, 2));
+            scoreboard.updateGameScore(game2, new Score(2, 0));
+            scoreboard.updateGameScore(game3, new Score(0, 4));
+
+            var scoreboardSummary = scoreboard.collectSummary();
+
+            assertEquals(3, scoreboardSummary.games().size());
+            assertEquals(game3, scoreboardSummary.games().get(0));
+            assertEquals(game1, scoreboardSummary.games().get(1));
+            assertEquals(game2, scoreboardSummary.games().get(2));
         }
     }
 }
